6e4dd0f2ba9e2757fc85c107725f352ed6a526bc
easysoftware2014 <ayandapatrick@gmail.com>
2018-08-03 14:54:25 +0200

    Add files via upload

---
+++ Insedlu.Implementation/NhibernateHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace Insedlu.Implementation
+{
+    static class NhibernateHelper
+    {
+        public static void Startup()
+        {
+            var configuartion = new Configuration();
+            configuartion.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = "<metadata=res://*/Connection.Model1.csdl|res://*/Connection.Model1.ssdl|res://*/Connection.Model1.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=.;initial catalog=Insendlu;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot; providerName=System.Data.EntityClient />";
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+            });
+        }
+    }
+}
---
+++ Insedlu.Implementation/TokenTemplate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Insedlu.Implementation
+{
+    public class TokenTemplate
+    {
+        private bool _scancomplete;
+        private string _body;
+        private readonly List<string> _tokens;
+        public TokenTemplate(string tokenFilename)
+        {
+            _scancomplete = false;
+            _body = string.Empty;
+            _tokens = new List<string>();
+            ReadBody(tokenFilename);
+        }
+        public string Body
+        {
+            set
+            {
+                _body = value;
+            }
+            get
+            {
+                return _body;
+            }
+        }
+        private void ReadBody(string tokenFilename)
+        {
+            using (var streamReader = new StreamReader(tokenFilename))
+            {
+                var result = string.Empty;
+
+                try
+                {
+                    string line;
+
+                    do
+                    {
+                        line = streamReader.ReadLine();
+                        result += line;
+                    }
+                    while (line != null);
+
+                    _body = result;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(string.Format("File {0} could not be read. <br><br>" + ex.StackTrace, tokenFilename));
+                }
+            }
+
+        }
+        public void SetTokenValue(string token, string value)
+        {
+            if (_scancomplete == false)
+            {
+                ScanDocument();
+            }
+            string searchToken = "%" + token + "%";
+            if (_body.IndexOf(searchToken, StringComparison.Ordinal) <= 0)
+                throw new ETokenNotFound(token);
+            _body = _body.Replace(searchToken, value);
+        }
+        public void ScanDocument()
+        {
+            var re = new Regex("%.*?%", RegexOptions.IgnoreCase);
+            var matchList = re.Matches(Body);
+            foreach (Match d in matchList)
+            {
+                if (!_tokens.Contains(d.Value))
+                {
+                    _tokens.Add(d.Value);
+                }
+            }
+            _scancomplete = true;
+        }
+    }
+
+    public class ETokenNotFound : Exception
+    {
+        public ETokenNotFound(string token): base("Token '" + token + "' not found.") { }
+        
+    }
+}
